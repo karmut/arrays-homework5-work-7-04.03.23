12754c0ba21acbe7a5804c9f403d22e0bc81e4a5
karmut <karina.mutafyan@gmail.com>
2023-03-07 18:45:35 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,29 @@
+let numbers = [23, 45, 6, 2, 15];
+console.log(length);
+
+//-------------------------------
+
+let names = [
+  "Karina",
+  "Maxsim",
+  "Tom",
+  "Alica",
+  "Michelle",
+  "Emma",
+  "Valeria",
+  "Mikael",
+  "Mina",
+  "lisa",
+];
+
+console.log(names[0], names[3], names[7]);
+
+//---------------------
+
+let Masiv = [];
+
+Masiv[1] = 10;
+Masiv[2] = 8;
+Masiv[5] = 100;
+
+console.log(Masiv);
